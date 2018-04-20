cb6232b7a85472f388a2b9a200b237be4a49bc6d
Quentin Désert <quentin.desert@schibsted.com>
2018-04-20 09:09:29 -0500

    added composite disposable

--- app/src/main/java/com/example/desert/roomwordsample/WordViewModel.kt
+++ app/src/main/java/com/example/desert/roomwordsample/WordViewModel.kt
@@ -2,10 +2,31 @@ package com.example.desert.roomwordsample
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 class WordViewModel(private val wordRepository: WordRepository) : ViewModel() {
 
+    private val compositeDisposable = CompositeDisposable()
+
     fun getAllWords() : LiveData<List<Word>>? = wordRepository.getAllWords()
 
-    fun insert(word: Word) = wordRepository.insert(word)
+    fun insert(word: Word) {
+        compositeDisposable.add(wordRepository.insert(word)
+                .subscribe({ insertSuccess() }, { insertError() }))
+    }
+
+    private fun insertSuccess() {
+        print("Insert success")
+    }
+
+    private fun insertError() {
+        print("Insert error")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+    }
 }
